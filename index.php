246ad5213807d733cbc53442774a32bdaf1ecbee
Ferry <allan.ferry@orange.fr>
2018-12-12 11:02:18 +0100

    <df

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
 <?php
+class Mere
+{
+  public function __construct()
+  {
+    echo static::class;
+  }
+}
+
+class Fille extends Mere
+{
+
+}
+
+new Fille;
+
 // On enregistre notre autoload.
 function chargerClasse($classname)
 {
