d49e25cb15cc22c7df7d2ae811111e18093120ae
Ferry <allan.ferry@orange.fr>
2018-12-17 11:39:37 +0100

    sd

--- Magicien.php
+++ Magicien.php
@@ -1,4 +1,6 @@
 <?php
 class Magicien extends Personnage {
-  
+    const CEST_MOI = 1; // Constante renvoyée par la méthode `frapper` si on se frappe soi-même.
+    const PERSONNAGE_TOUCHE = 2; // Constante renvoyée par la méthode `frapper` si on a tué le personnage en le frappant.
+
 }
